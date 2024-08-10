3211c03e328319cbbdca963885f6fae9603ecb9a
Hatim <hatimhasbullah@gmail.com>
2024-08-10 22:03:59 +0800

    added listen on port

--- main.py
+++ main.py
@@ -246,4 +246,5 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to port 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
